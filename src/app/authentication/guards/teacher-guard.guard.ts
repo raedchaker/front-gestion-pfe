fb2bab043cfd3b882f4f7cc447ced2e071aeb947
ines ouled elhaj <ines.ouledelhaj@insat.u-carthage.tn>
2021-02-07 20:38:39 +0100

    sidebar changes

--- src/app/authentication/guards/teacher-guard.guard.ts
+++ src/app/authentication/guards/teacher-guard.guard.ts
@@ -1,27 +1,37 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { AuthenticationService } from '../authentication.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TeacherGuardGuard implements CanActivate {
   constructor(
-    private auth: AuthenticationService, 
+    private auth: AuthenticationService,
     private router: Router,
-    private toastr:ToastrService
-  ){}
+    private toastr: ToastrService
+  ) {}
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      if(this.auth.getRole()!=='student'){
-        this.router.navigate(['/subject/list-pfe']);
-        this.toastr.error("You need to be teacher ")
-        return false;
-      }
-      return true;
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (this.auth.getRole() !== 'teacher') {
+      this.router.navigate(['/subject/list-pfe']);
+      this.toastr.error('You need to be teacher ');
+      return false;
+    }
+    return true;
   }
-  
 }
